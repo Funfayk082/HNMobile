771503d2c3a716aaa7e0a7b88222d921e3df5b14
Funfayk082 <82674237+Funfayk082@users.noreply.github.com>
2024-04-20 20:03:05 +0300

    feat: Наверстал авторизацию и регистрацию

--- app/src/main/java/com/example/helpnearbymobile/presentation/auth_ui/RegisterFragment.kt
+++ app/src/main/java/com/example/helpnearbymobile/presentation/auth_ui/RegisterFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.helpnearbymobile.R
+import com.example.helpnearbymobile.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +15,10 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentRegisterBinding.bind(view)
     }
 }
